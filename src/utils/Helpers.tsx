e302cb54fdbf9d4c1db956baede314d2a773ecf2
sleekcharly <c_ukasoanya@yahoo.com>
2025-01-10 01:32:18 +0100

    Customized application file and folder structure

---
+++ src/utils/Helpers.tsx
@@ -0,0 +1,8 @@
+import { Href, Router, router } from 'expo-router';
+
+export const resetAndNavigate = (newPath: Href) => {
+  if (router.canGoBack()) {
+    router.dismissAll();
+  }
+  router.replace(newPath);
+};
