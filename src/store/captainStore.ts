449ae7546e259fdb94cd6d68a21e46592b51f5f7
sleekcharly <c_ukasoanya@yahoo.com>
2025-01-11 17:18:01 +0100

    created the zustand captain and user store

---
+++ src/store/captainStore.ts
@@ -0,0 +1,41 @@
+import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { mmkvStorage } from './storage';
+
+type CustomLocation = {
+  latitude: number;
+  longitude: number;
+  address: string;
+  heading: number;
+} | null;
+
+interface CaptainStoreProps {
+  user: any;
+  location: CustomLocation;
+  onDuty: boolean;
+  setUser: (data: any) => void;
+  setOnDuty: (data: boolean) => void;
+  setLocation: (data: CustomLocation) => void;
+  clearData: () => void;
+}
+
+export const useCaptainStore = create<CaptainStoreProps>()(
+  persist(
+    (set) => ({
+      user: null,
+      location: null,
+      onDuty: false,
+      setUser: (data) => set({ user: data }),
+      setLocation: (data) => set({ location: data }),
+      setOnDuty: (data) => set({ onDuty: data }),
+      clearData: () => set({ user: null, location: null, onDuty: false }),
+    }),
+    {
+      name: 'captain-store',
+      partialize: (state) => ({
+        user: state.user,
+      }),
+      storage: createJSONStorage(() => mmkvStorage),
+    },
+  ),
+);
