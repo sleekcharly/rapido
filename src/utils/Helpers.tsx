4ad3e2304bfb168af61275dbe991c390d0bc7bff
sleekcharly <c_ukasoanya@yahoo.com>
2025-01-12 13:10:15 +0100

    cleared app data

--- src/utils/Helpers.tsx
+++
@@ -1,8 +0,0 @@
-import { Href, Router, router } from 'expo-router';
-
-export const resetAndNavigate = (newPath: Href) => {
-  if (router.canGoBack()) {
-    router.dismissAll();
-  }
-  router.replace(newPath);
-};
