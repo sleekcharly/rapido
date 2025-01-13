a05b2526eb6f25722d710b633d0a8dd51ef62a4e
sleekcharly <c_ukasoanya@yahoo.com>
2025-01-13 14:09:36 +0100

    created the role page and customer/ captain auth pages

---
+++ src/app/captain/auth.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <View>
+      <Text>Captain</Text>
+    </View>
+  );
+};
+
+export default Auth;
