e1cc21341a9b70ab387a87face9c81b87679eada
sleekcharly <c_ukasoanya@yahoo.com>
2025-01-14 21:26:04 +0100

    set up apiinterceptors functionality

--- src/service/apiinterceptors.ts
+++ src/service/apiinterceptors.ts
@@ -27,3 +27,30 @@ export const refresh_token = async () => {
     logout();
   }
 };
+
+appAxios.interceptors.request.use(async (config) => {
+  const accessToken = tokenStorage.getString('access_token');
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+});
+
+appAxios.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if (error.response && error.response.status === 401) {
+      try {
+        const newAccessToken = await refresh_token();
+        if (newAccessToken) {
+          error.config.headers.Authorization = `Bearer ${newAccessToken}`;
+          return axios(error.config);
+        }
+      } catch (error) {
+        console.log('Error refreshing token');
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
